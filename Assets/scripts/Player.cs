331bb3b8fb5b16321eabf7d38a9e08a37060c208
moi <not@me>
2023-07-29 22:52:39 +0200

    finish animation player, disable rotation on Z axe

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -9,40 +9,24 @@ public class Player : MonoBehaviour
 
     private const float speed = 300;
     private Vector2 vectorMovement;
-    private string currentState = "playerIdleFront";
 
     void Update()
     {
         vectorMovement.x = Input.GetAxisRaw("Horizontal");
         vectorMovement.y = Input.GetAxisRaw("Vertical");
-        Debug.Log(animator.GetCurrentAnimatorStateInfo(1));
         if (vectorMovement != Vector2.zero) {
             animator.SetFloat("X", vectorMovement.x);
             animator.SetFloat("Y", vectorMovement.y);
-        } else {        
-                if (currentState == "playerRunFront") {
-                    ChangeAnimationState("playerIdleFront");
-                } else if (currentState == "playerRunBack") {
-                    ChangeAnimationState("playerIdleBack");
-                } else if (currentState == "playerRunLeft") {
-                    ChangeAnimationState("playerIdleLeft");
-                } else if (currentState == "playerRunRight") {
-                    ChangeAnimationState("playerIdleRight");
-                } 
+        } else {
+            float lastX = animator.GetFloat("X");
+            float lastY = animator.GetFloat("Y");
+            animator.SetFloat("X", lastX / 2);
+            animator.SetFloat("Y", lastY / 2);
         }
         /*
         normalize : avoid acceleration if you walk diagonally
         Time.deltaTime : avoid people who have a better frame rate to go faster
         */
         rigidbody.velocity = vectorMovement.normalized * Time.deltaTime * speed;
-    }
-
-    void ChangeAnimationState(string newState) {
-        if (currentState ==  newState) {
-            return ;
-        } else {
-            animator.Play(newState);
-            currentState = newState;
-        }
     }
 }
