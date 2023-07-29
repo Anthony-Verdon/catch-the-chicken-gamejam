71cd520f9af7e7e2c12b61636036c6b79e7ce0d9
moi <not@me>
2023-07-29 21:33:21 +0200

    finish animations player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,27 +5,54 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidbody;
-    private Vector2 vectorMovement;
+    [SerializeField] private Animator animator;
+
     private const float speed = 300;
+    private Vector2 vectorMovement;
+    private string currentState = "playerIdleFront";
 
     void Update()
     {
         vectorMovement.x = Input.GetAxisRaw("Horizontal");
         vectorMovement.y = Input.GetAxisRaw("Vertical");
 
-        Debug.Log("x:" + vectorMovement.x + "y:" + vectorMovement.y);
-        if (vectorMovement.x < 0) {
+        //d = x:1, a = x:-1, w = y:1, s = y:-1
+        //Debug.Log("x:" + vectorMovement.x + "y:" + vectorMovement.y);
+        if (vectorMovement.x == 0 && vectorMovement.y == 0) {
+            if (currentState == "playerRunFront") {
+                ChangeAnimationState("playerIdleFront");
+            } else if (currentState == "playerRunBack") {
+                ChangeAnimationState("playerIdleBack");
+            } else if (currentState == "playerRunLeft") {
+                ChangeAnimationState("playerIdleLeft");
+            } else if (currentState == "playerRunRight") {
+                ChangeAnimationState("playerIdleRight");
+            } 
         } else {
-
-        }
-        if (vectorMovement.y < 0) {
-        } else {
-            
+            if (vectorMovement.x < 0) {
+                ChangeAnimationState("playerRunLeft");
+            } else if (vectorMovement.x != 0) {
+                ChangeAnimationState("playerRunRight");
+            }
+            if (vectorMovement.y < 0) {
+                ChangeAnimationState("playerRunFront");
+            } else if (vectorMovement.y != 0) {
+                ChangeAnimationState("playerRunBack");
+            }
         }
         /*
         normalize : avoid acceleration if you walk diagonally
         Time.deltaTime : avoid people who have a better frame rate to go faster
         */
         rigidbody.velocity = vectorMovement.normalized * Time.deltaTime * speed;
+    }
+
+    void ChangeAnimationState(string newState) {
+        if (currentState ==  newState) {
+            return ;
+        } else {
+            animator.Play(newState);
+            currentState = newState;
+        }
     }
 }
